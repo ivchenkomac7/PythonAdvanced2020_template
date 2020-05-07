c28aa7d9229e25d522e5997cec8e3cdeac08b6d3
mac7 <ivchenko.mac7@gmail.com>
2020-05-07 18:29:02 +0300

    updated

--- less3/3_1.py
+++ less3/3_1.py
@@ -2,21 +2,25 @@
 # определенное кол-во раз, будет выводить кол-во времени
 # затраченного на выполнение данной функции и её название.
 
-
 from datetime import datetime
 
 
-def decorator(func):
-    def wrapper(*args, **kwargs):
-        for i in range(1, 11):
-            start = datetime.now()
-            result = func(*args, **kwargs)
-            print(f"The function '{func.__name__}' runs -{i}- times, result = {result}")
-            print(f"It takes {datetime.now() - start}")
-    return wrapper
+def decorator(arg):
+    print(arg)
+
+    def inner(func):
+
+        def wrapper(*args, **kwargs):
+            for i in range(1, 11):
+                start = datetime.now()
+                result = func(*args, **kwargs)
+                print(f"The function '{func.__name__}' runs -{i}- times, result = {result}")
+                print(f"It takes {datetime.now() - start}")
+        return wrapper
+    return inner
 
 
-@decorator
+@decorator("Hello")
 def exponent(a, b):
     return a**b
 
